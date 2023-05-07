4350cb07983d4ac944e6ed135e27540c912b2439
LDH1103 <ldh517525@gmail.com>
2023-05-07 22:45:42 +0900

    23-05-07

--- num_processing.py
+++ num_processing.py
@@ -4,7 +4,7 @@ print( max( 5, 12 ) ) # 최대값 : max
 print( max( 5, 12 ) ) # 최소값 : min
 print( round( 3.14 ) ) # 반올림 : round
 
-from math import * # math 라이브러리 안의 모즌것을 이용하겠다
+from math import * # math 라이브러리 안의 모든것을 이용하겠다
 print( floor( 4.99 ) ) # 내림 : floor
 print( ceil( 3.14 ) ) # 올림 : ceil
 print( sqrt( 16 ) ) # 제곱근 : sqrt
